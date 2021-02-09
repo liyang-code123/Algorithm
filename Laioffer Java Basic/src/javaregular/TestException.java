4caf28c20db66446077bb69623ead96b80e3e3bd
liyang-code123 <liyang.code@gmail.com>
2021-02-09 01:45:23 -0500

    Converted to IDE and Exceptions

--- Laioffer Java Basic/src/javaregular/TestException.java
+++ Laioffer Java Basic/src/javaregular/TestException.java
@@ -2,6 +2,24 @@ package javaregular;
 
 public class TestException {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		try {
+			int a = 0;
+			System.out.println("a =" + a);
+			int b = 20 / a;
+			System.out.println("Division done.");
+			System.out.println("b=" + b);
+		} catch(ArithmeticException e) {
+			System.out.println("Divide by zero error");
+		} finally {
+			System.out.println("inside the finally block");
+		}
+		System.out.println("after the finally block");
+//		try {
+//
+//		} catch (Exception e) {
+//
+////		} catch (ArrayIndexOutOfBoundsException e) {
+////
+////		}
 	}
 }

9ad37a475565fd0e87d9e74da820bd0a2e5fda6b
liyang-code123 <liyang.code@gmail.com>
2021-04-13 23:43:48 -0400

    DeepCopyLinkedList2 - Space O(1) Time O(n)

---
+++ Laioffer Java Basic/src/algorithm/DeepCopyLinkedList2.java
@@ -0,0 +1,44 @@
+package algorithm;
+
+public class DeepCopyLinkedList2 {
+    public class Node {
+        Node next;
+        Node random;
+        int value;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        // Step 1: Creating a new list of original and new A->A'->B->B'->C->C'
+        Node cur = head;
+        while(cur != null) {
+            Node newNode = new Node(cur.value);
+            // insert the cloned node to the next of original
+            newNode.next = cur.next;
+            cur.next = newNode;
+            cur = newNode.next;
+        }
+        cur = head;
+
+        // Step 2: link the random of the new node
+        while(cur != null) {
+            cur.next.random = (cur.random != null) ? cur.random.next : null;
+            cur = cur.next.next;
+        }
+
+        // Step 3: separate original and new linked list
+        Node cur_old_list = head;
+        Node cur_new_list = head.next;
+        Node head_new = head.next; // used for return new head.
+        while(cur_old_list != null) {
+            cur_old_list.next = cur_old_list.next.next;
+            cur_new_list.next = (cur_new_list.next != null) ? cur_new_list.next.next : null;
+            cur_old_list = cur_old_list.next;
+            cur_new_list = cur_new_list.next;
+        }
+        return head_new;
+    }
+}

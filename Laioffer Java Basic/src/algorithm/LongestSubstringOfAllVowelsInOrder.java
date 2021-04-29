71beed312858194599880c07665d2eceda96d522
liyang-code <liyang.code@gmail.com>
2021-04-28 21:13:38 -0700

    LeetCode 1839

---
+++ Laioffer Java Basic/src/algorithm/LongestSubstringOfAllVowelsInOrder.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+// LeetCode 1839
+public class LongestSubstringOfAllVowelsInOrder {
+    public static int longest(String word) {
+        // corner case
+        if (word == null || word.length() < 5) return 0;
+
+        int beatiCount = 0;
+        int vCount = 1;
+        int res = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (i != 0 && word.charAt(i - 1) > word.charAt(i) ) {
+                vCount = 0;
+                beatiCount = 0;
+            }
+            beatiCount++;
+
+            if (i != 0 && word.charAt(i - 1) != word.charAt(i)) {
+                vCount++;
+            }
+
+            if (vCount == 5) {
+                res = Math.max(res, beatiCount);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String word = "aeiouuuuuu";
+        System.out.println(LongestSubstringOfAllVowelsInOrder.longest(word));
+    }
+}

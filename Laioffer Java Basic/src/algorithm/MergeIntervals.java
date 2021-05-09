8c51e583933acf5433100ebba11715cc55778987
liyang-code <liyang.code@gmail.com>
2021-05-09 00:03:01 -0700

    Update MergeIntervals LeetCode 56

--- Laioffer Java Basic/src/algorithm/MergeIntervals.java
+++ Laioffer Java Basic/src/algorithm/MergeIntervals.java
@@ -22,10 +22,11 @@ public class MergeIntervals {
         return result.toArray(new int[result.size()][]);
     }
 
-    public static  void main (String[] args) {
+    public static void main (String[] args) {
         MergeIntervals test = new MergeIntervals();
         int[][] intervals= {{1, 2}, {1, 4}, {3,4}, {2, 3}};
-        for (int[] interval : )
-        System.out.println(test.mergeIntervals(intervals));
+        for (int i = 0; i < 2; i++) {
+            System.out.println(Arrays.toString(test.mergeIntervals(intervals)[i]));
+        }
     }
 }

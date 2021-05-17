25b77058c6dfac8c3b68a273c1586ba013d2bf5b
liyang-code <liyang.code@gmail.com>
2021-05-16 23:55:39 -0700

    ArrayHopperI LaiCode 88

--- Laioffer Java Basic/src/algorithm/ArrayHopper.java
+++ Laioffer Java Basic/src/algorithm/ArrayHopper.java
@@ -1,4 +1,24 @@
 package algorithm;
 
+//LaiCode 88
 public class ArrayHopper {
+    public boolean canJump(int[] array) {
+        boolean[] dp = new boolean[array.length];
+        dp[0] = true;
+
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i + 1; j <= array.length; j++) {
+                if (dp[i] && j <= i + array[i]) {
+                    dp[j] = true;
+                }
+            }
+        }
+        return dp[array.length - 1];
+    }
+
+    public static void main (String[] args) {
+        ArrayHopper test = new ArrayHopper();
+        int[] array = {1, 2, 0, 3, 4, 1};
+        System.out.println(test.canJump(array));
+    }
 }

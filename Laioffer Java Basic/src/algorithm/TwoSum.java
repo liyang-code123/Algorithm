7c551a48188569ebbdba36a542add64fe855bd3f
liyang-code <liyang.code@gmail.com>
2021-05-25 23:19:39 -0700

    TwoSum LeetCode 1

--- Laioffer Java Basic/src/algorithm/TwoSum.java
+++ Laioffer Java Basic/src/algorithm/TwoSum.java
@@ -1,9 +1,30 @@
 package algorithm;
 
-import java.util.List;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+// LeetCode 1
 public class TwoSum {
-//    public List<List<Integer>> allPairs(int[] array, int target) {
-//
-//    }
+    public int[] twoSum(int[] array, int target) {
+        if (array == null || array.length == 0) return new int[]{-1, -1};
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int remaining = target - array[i];
+            if (map.containsKey(remaining)) {
+                return new int[]{map.get(remaining), i};
+            }
+            map.put(array[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        TwoSum test = new TwoSum();
+        int[]array = {1, 2, 2, 3, 4, 5, 5, 6};
+        int target = 8;
+        System.out.println(Arrays.toString(test.twoSum(array, target)));
+    }
 }

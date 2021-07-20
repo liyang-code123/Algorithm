c578f2daf75e9c019369625d5c31bbd691966c0a
liyang-code <liyang.code@gmail.com>
2021-07-19 22:04:19 -0700

    test

--- Laioffer Java Basic/src/algorithm/Solution.java
+++ Laioffer Java Basic/src/algorithm/Solution.java
@@ -1,32 +1,19 @@
-//public class Solution {
-//    public List<String> permutations(String input) {
-//        List<String> res = new ArrayList<>();
-//        if (input.length() == 0) {
-//            res.add("");
-//            return res;
-//        }
-//
-//        char[] array = input.toCharArray();
-//        dfs(array, 0, res);
-//        return res;
-//    }
-//
-//    private void dfs(char[] array, int position, List<String> res) {
-//        if (position == array.length) {
-//            res.add(new String(array));
-//            return;
-//        }
-//
-//        for (int i = position; i < array.length; i++) {
-//            swap(array, i, position);
-//            dfs(array, position + 1, res);
-//            swap(array, i, position);
-//        }
-//    }
-//
-//    private void swap(char[] array, int i, int j) {
-//        char temp = array[i];
-//        array[i] = array[j];
-//        array[j] = temp;
-//    }
-//}
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public String remove(String input, String t) {
+        Set<Character> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t.length(); ++i) {
+            set.add(t.charAt(i));
+        }
+
+        for (int i = 0; i < input.length(); ++i) {
+            if (!set.contains(input.charAt(i))) {
+                sb.append(input.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}

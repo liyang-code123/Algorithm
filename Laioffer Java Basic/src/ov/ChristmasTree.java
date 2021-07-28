50cadfe85ec6cb3cbdc74a607e7c8ea7dbe4b832
liyang-code <liyang.code@gmail.com>
2021-07-27 23:42:40 -0700

    Christmas Tree

---
+++ Laioffer Java Basic/src/ov/ChristmasTree.java
@@ -0,0 +1,29 @@
+package ov;
+
+//public class ChristmasTree {
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < 10 - i; j++) {
+//                System.out.println(" ");
+//            }
+//            for (int k = 0; k < (2 * i + 1); k++) {
+//                System.out.println("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+public class ChristmasTree {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10 - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k < (2 * i + 1); k++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}

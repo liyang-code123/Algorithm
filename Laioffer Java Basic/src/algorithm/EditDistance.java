c316b96f98485e5ebb22d845e4e44a58eb26a0a1
liyang-code <liyang.code@gmail.com>
2021-08-09 22:41:01 -0700

    Edit Distance DP Problem

---
+++ Laioffer Java Basic/src/algorithm/EditDistance.java
@@ -0,0 +1,33 @@
+public class EditDistance {
+    public int editDistance(String s, String t) {
+        if (s == null || t == null || s.length() == 0 || t.length() == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= t.length(); j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i - 1) != t.charAt(j - 1)) {
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
+    }
+
+    public static void main(String[] args) {
+        EditDistance test = new EditDistance();
+        System.out.println(test.editDistance("aaab", "b"));
+    }
+}

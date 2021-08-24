661d492a397a023fb378ef0a1baa47458bf7ce75
liyang-code <liyang.code@gmail.com>
2021-08-23 23:20:01 -0700

    laioffer binary search

--- Laioffer Java Basic/src/javaregular2021/BinarySearch.java
+++ Laioffer Java Basic/src/javaregular2021/BinarySearch.java
@@ -4,4 +4,35 @@ public class BinarySearch{
     public int classicBinarySearch(int[] arr, int target) {
         return -1;
     }
+    public int[] classicalBinarySearchIn2DSpace(int[][] arr, int target) {
+
+    }
+
+    public int closestElementToTarget(int[] arr, int target) {
+
+    }
+
+    public int firstTarget(int[] arr, int target) {
+
+    }
+
+    public int lastTarget(int[] arr, int target) {
+
+    }
+
+    public int closestKthElement(int arr, int target, int k) {
+
+    }
+
+    public int smallestElementThatIsLargestThanTarget(int[] arr, int target) {
+
+    }
+
+    public int kthSmallestInTwoSortedArrays(int[] arr1, int[] arr2, int k) {
+
+    }
+
+    public boolean binarySearchWithUnknownSize(int[] arr, int target) {
+
+    }
 }
